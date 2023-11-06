9e56ce1b908082ba3a70205ae198f6f2b0ab96ae
LEESY1004 <leesunyeok@gmail.com>
2023-11-06 20:54:16 +0900

    2023.11.06 최종

--- objects/result.py
+++ objects/result.py
@@ -16,12 +16,12 @@ class Result:
         print("플레이어의 카드:", playgame.player_hand)
         print("딜러의 카드:", playgame.dealer_hand)
 
-        if player_value > 21 or (dealer_value <= 21 and dealer_value >= player_value):
+        if dealer_value == player_value or (dealer_value > 21 and player_value > 21):
+            print("무승부: 플레이어 {} vs. 딜러 {}".format(player_value, dealer_value))
+        elif player_value > 21 or (dealer_value <= 21 and dealer_value >= player_value):
             print("패배: 플레이어 {} vs. 딜러 {}".format(player_value, dealer_value))
         elif dealer_value > 21 or (player_value <= 21 and player_value > dealer_value):
             print("승리: 플레이어 {} vs. 딜러 {}".format(player_value, dealer_value))
-        else:
-            print("무승부: 플레이어 {} vs. 딜러 {}".format(player_value, dealer_value))
 
 result = Result()
 result.result(player_value, dealer_value)
